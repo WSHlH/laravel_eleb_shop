6d048af31c0f21db25ddcbeff12725503c5f11e7
WSHlH <1764674098@qq.com>
2018-04-19 21:49:09 +0800

    注册店铺,店铺登录

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::resource('business','BusinessController');
+Route::resource('businessList','BusinessListController');
+
+//店铺登录与退出
+Route::get('login','LoginController@show')->name('login');
+Route::post('login','LoginController@store')->name('login');
+Route::delete('logout','LoginController@destroy')->name('logout');
